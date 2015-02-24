34b97bfada1ba16629a75de13f50da010a86f7fb
Daniel St. Clair <danielallanstclair@gmail.com>
2015-02-24 15:22:38 -0600

    finished hard mode! have states plugged into api and getting data from there. filtering and sorting is functional.

---
+++ scripts/controllers.js
@@ -0,0 +1,29 @@
+angular.module('app.controller', []).controller('statesControl', function($scope, $http){
+	$scope.states = [];
+	$scope.statesCopy = [];
+	var promise = $http.get('http://tiny-pizza-server.herokuapp.com/collections/fancy-table')
+	.success(function(response){
+		for(var i=0; i<response.length; i++){
+			if(response[i].name && response[i].abbreviation){
+				$scope.states.push(response[i]);
+				$scope.statesCopy.push(response[i]);
+			}
+		}
+	}).error(function(err){
+		console.log(err);
+		});
+
+	$scope.stateFlip = function(){
+		$scope.statesCopy.reverse();
+	}
+	$scope.filter= "";
+	$scope.$watch('filter', 
+		function() {
+			console.log('first filter letter: ' + $scope.filter[0]);
+			$scope.statesCopy = _.filter($scope.states,
+				function(element){
+					console.log('filter inside: ' + $scope.filter);
+					return element.name.toLowerCase().indexOf($scope.filter.toLocaleLowerCase()) > -1 || element.abbreviation.toLowerCase().indexOf($scope.filter.toLocaleLowerCase()) > -1;
+		});
+	});
+})
